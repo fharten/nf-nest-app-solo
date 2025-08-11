798637afa1b4ab5e7f7bfb44aeef8c8bfe9b8ee5
fharten <florian.harten@pm.me>
2025-08-11 16:00:20 +0200

    removed explixit cors config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Service that gives a list of quotes back as json
+// Service that gives a single quote back as json
+
+// Controller for a Route that returns a random quote
+// Controller for a Route that returns all quotes
+
+// basic server functionality
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'http://localhost:3000', // Or the origin of your frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false, // If your API needs to handle cookies or authentication
-  });
-
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(3232);
+  console.log(`Server ist running on port ${await app.getUrl()}`);
 }
+
 bootstrap();
