cca2bb785e2ab51138c2ebbd343547384f07ceed
fharten <florian.harten@pm.me>
2025-08-11 16:07:49 +0200

    added static react page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuoteModule } from './quotes/quotes.module';
@@ -15,6 +17,9 @@ dotenv.config();
   controllers: [AppController],
   providers: [AppService],
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     QuoteModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
